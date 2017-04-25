26372903c7f160d0abd213645f321a9fc14f442f
Laura Wolfart <laurawolfart@gmail.com>
2017-04-25 15:27:53 -0400

    landing page layout started

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { Input } from './components/common'
 
 export default class LandingPage extends Component {
+  state = {
+    currentConnection: ''
+  }
 
-
+  grabBusiness(value) {
+    this.setState({currentConnection: value});
+  }
 
   render() {
     return (
-      <View style={{margin: 128}}>
+      <View style={styles.container}>
+        <Text style={styles.header2}>Why fight the crowd, when you can order your drinks without missing a beat.</Text>
+        <Text style={styles.label}>Connect to a Business!</Text>
+        <TextInput
+        placeholder="Business Id"
+        label="Connect"
+        value={this.state.currentConnection}
+        style ={styles.inputform}
+        onChangeText={this.grabBusiness.bind(this)} />
         <Text onPress={Actions.dashboard}>This is LandingPage!</Text>
       </View>
-    )
+    );
   }
+}
+
+
+
+const styles = {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 100
+  },
+  header2: {
+    color: '#000000',
+    fontWeight: 'bold',
+    alignItems: 'center',
+    flex: 1
+  },
+  label: {
+    color: '#000000',
+    fontWeight: 'bold',
+    alignItems: 'center',
+    flex: 1
+  },
+  inputform: {
+    textAlign: 'left',
+    color: '#333333',
+    margin: 5,
+    height: 50,
+    borderColor: '#60b7e2',
+    borderWidth: 1,
+  }
+
 }
