ca9047e8f958f3b064e45875f6c9309a45027886
James Guthrie <jamesrguthrie@gmail.com>
2017-04-25 21:27:29 -0400

    added styles to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Actions, ActionConst } from 'react-native-router-flux';
 import { Input, Button } from './common';
 import { connect } from 'react-redux';
@@ -36,28 +36,35 @@ export class LandingPage extends Component {
   errorMessage() {
     if (this.state.errorMessage) {
       return (
-      <Text>
-        Connection Error
-      </Text>
-    );
+        <Text style={styles.errorMessage}>
+          Connection Error
+        </Text>
+      );
     }
-
     return <Text></Text>
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header2}>Why fight the crowd, when you can order your drinks without missing a beat.</Text>
-        <Text style={styles.label}>Connect to a Business!</Text>
-        {this.errorMessage()}
-        <TextInput
-        placeholder="Business Id"
-        label="Connect"
-        value={this.state.currentConnection}
-        style ={styles.inputform}
-        onChangeText={this.grabBusiness.bind(this)} />
-        <Button onPress={() => this.onConnect()}/>
+        <View style={styles.header}>
+          <Text style={styles.topHeader}>Q-less. Why wait in a line.</Text>
+          <Text style={styles.secondaryHeader}>Connect to a Business!</Text>
+        </View>
+        <View>
+          {this.errorMessage()}
+          <TextInput
+          placeholder="Business Id"
+          label="Connect"
+          style={styles.textInput}
+          value={this.state.currentConnection}
+          onChangeText={this.grabBusiness.bind(this)} />
+        <TouchableOpacity style={styles.connectButton} onPress={() => this.onConnect()}>
+          <Text style={styles.connectButtonText}>
+            Connect!
+          </Text>
+        </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -65,30 +72,84 @@ export class LandingPage extends Component {
 
 const styles = {
   container: {
-    flex: 1,
+    flex: 0,
     flexDirection: 'column',
-    paddingTop: 100
+    paddingTop: 100,
   },
-  header2: {
-    color: '#000000',
+  header: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  topHeader: {
+    marginTop: 20,
+    padding: 10,
     fontWeight: 'bold',
-    alignItems: 'center',
-    flex: 1
+    fontSize: 18,
+    marginBottom: 50,
+    textAlign: 'center',
+    justifyContent: 'center'
   },
-  label: {
-    color: '#000000',
-    fontWeight: 'bold',
-    alignItems: 'center',
-    flex: 1
+  secondaryHeader: {
+    marginTop: 35,
+    padding: 10,
+    textAlign: 'center'
   },
-  inputform: {
-    textAlign: 'left',
-    color: '#333333',
-    margin: 5,
-    height: 50,
-    borderColor: '#60b7e2',
+  textInput: {
+    marginTop: 10,
+    fontSize: 12,
+    height: 35,
+    width: 220,
+    borderColor: '#000',
     borderWidth: 1,
+    borderRadius: 10,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+  connectButton: {
+    marginTop: 10,
+    height: 35,
+    width: 220,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+  connectButtonText: {
+    textAlign: 'center'
+  },
+  errorMessage: {
+    textAlign: 'center',
+    color: 'red'
   }
+
+  // header: {
+  //   color: '#000000',
+  //   margin: 0,
+  //   alignSelf: 'center',
+  //   justifyContent: 'center',
+  //   textAlign: 'center',
+  //   padding: 10,
+  //   fontWeight: 'bold',
+  //   alignItems: 'center',
+  //   flex: 1
+  // },
+  // label: {
+  //   color: '#000000',
+  //   fontWeight: 'bold',
+  //   textAlign: 'center',
+  //   alignItems: 'center',
+  //   flex: 1
+  // },
+  // inputform: {
+  //   textAlign: 'left',
+  //   color: '#333333',
+  //   margin: 5,
+  //   height: 50,
+  //   borderColor: '#60b7e2',
+  //   borderWidth: 1,
+  // }
 }
 
 export default connect()(LandingPage);
